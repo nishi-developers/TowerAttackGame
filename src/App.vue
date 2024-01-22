6a1e218951cb520b87a04679520ab62163f62b5c
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-01-23 00:39:12 +0900

    FirstComit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="MainCanvas">
+  <img src="@/assets/tou.png">
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+#MainCanvas {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: gray;
+}
+</style>
+<style>
+html,
+body,
+div#app {
+  height: 100%;
+  width: 100%;
+}
+</style> 
