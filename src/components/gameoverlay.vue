af22bb862243d0c6e7604af7436b254971bc3d33
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-02-22 19:57:03 +0900

    画面表示の大幅な改善

--- src/components/gameoverlay.vue
+++ src/components/gameoverlay.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="GameStart" class="overlay" v-if="props.step == 'GameStart'">
-        <p class="title"><b>{{props.stagename}}</b></p>
+        <p class="title"><b>[ {{ stagename }} ]</b></p>
         <div class="menus">
             <div class="box" @click="Action('GameStart')">
                 <p class="btext"><b>Start</b></p>
@@ -9,23 +9,58 @@
     </div>
     <div id="GameOver" class="overlay" v-if="props.step == 'GameOver'">
         <p class="title"><b>Game Over</b></p>
-    </div>
-    <div id="StageClear" class="overlay" v-if="props.step == 'StageClear'">
-        <p class="title"><b>Stage Clear</b></p>
         <div class="menus">
             <RouterLink to="/menu" class="RouterLink">
                 <div class="box">
                     <p class="btext">ステージセレクトに戻る</p>
                 </div>
             </RouterLink>
+            <div class="box" @click="Restart()">
+                <p class="btext">やり直す</p>
+            </div>
+        </div>
+    </div>
+    <div id="StageClear" class="overlay" v-if="props.step == 'StageClear'">
+        <p class="title"><b>Stage Clear</b></p>
+        <div class="menus">
+            <a :href="'/game/' + nextStageID" class="RouterLink">
+                <div class="box">
+                    <p class="btext">次のステージへ</p>
+                </div>
+            </a>
+            <!-- <RouterLink :to="'/game/' + nextStageID" class="RouterLink">
+                <div class="box">
+                    <p class="btext">次のステージへ</p>
+                </div>
+            </RouterLink> -->
+            <RouterLink to="/menu" class="RouterLink">
+                <div class="box">
+                    <p class="btext">ステージセレクトに戻る</p>
+                </div>
+            </RouterLink>
+            <div class="box" @click="Restart()">
+                <p class="btext">やり直す</p>
+            </div>
         </div>
     </div>
 </template>
 <script setup>
-const props = defineProps(["step","stagename"]) //親コンポーネントからデータなどを取得
+import StageData from "@/assets/StageData.json"
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+
+
+const props = defineProps(["step", "stageid"]) //親コンポーネントからデータなどを取得
 const emit = defineEmits(["re"]) //親コンポーネントから返答用の要素を取得
 function Action(action) { // タワーのクリックした位置を親コンポーネントに送信
     emit("re", action)
+}
+const stagename = StageData[props.stageid]['StageName']
+const nextStageID = String(Number(props.stageid) + 1)
+
+function Restart() {
+    router.go({ path: route.path, force: true })
 }
 </script>
 <style scoped>
@@ -51,67 +86,101 @@ function Action(action) { // タワーのクリックした位置を親コンポ
     top: 30%;
     left: 50%;
     transform: translateY(-50%) translateX(-50%);
-    font-size: 6rem;
     text-align: center;
-    width: 80%;
-    /* margin: 50% 0 0% 0; */
-
+    width: 100%;
 }
 
 .menus {
     position: absolute;
-    bottom: 30%;
+    top: 50%;
     left: 50%;
-    transform: translateY(-50%) translateX(-50%);
-    height: 10%;
+    transform: translateX(-50%);
+    height: 45%;
     width: 80%;
-    /* border: 10px #000 solid; */
+    /* background-color: #000; */
 }
 
 .menus .box {
-    border: 1px #000 solid;
-    border-radius: 10px;
-    height: 100%;
+    border-radius: 15px;
     position: relative;
+    box-sizing: border-box;
 }
 
-.menus .btext{
-    /* text-align: center; */
+.menus .btext {
     position: absolute;
     top: 50%;
-    left: 50%;
-    transform: translateY(-50%) translateX(-50%); 
+    transform: translateY(-50%);
+    width: 100%;
+    text-align: center;
 }
 
 /* GameStart */
-#GameStart .menus .box {
-    width: 50%;
-    margin: auto;
-    background-color: rgb(8, 164, 255);    
+/* タイトル */
+#GameStart .title {
+    font-size: 3rem;
+    color: #000000;
 }
 
+/* ボタンの枠 */
+#GameStart .menus .box {
+    width: 100%;
+    height: 35%;
+    margin: 10px auto;
+    border: 1px #000 solid;
+    background-color: rgb(35, 174, 255);
+}
 
+/* ボタンの文字 */
 #GameStart .menus .btext {
+    font-size: 4rem;
     color: white;
-    -webkit-text-stroke: 3px rgb(255, 230, 0);
-    font-size: 3rem;
+    /* -webkit-text-stroke: 3px rgb(255, 230, 0); */
 }
 
 /* GameOver */
-
+/* タイトル */
 #GameOver .title {
-    color: black;
+    font-size: 5.5rem;
+    color: #ca0000;
+}
 
+/* ボタンの枠 */
+#GameOver .menus .box {
+    width: 100%;
+    height: 25%;
+    margin: 10px auto;
+    border: 1px #000 solid;
+    background-color: rgb(35, 174, 255);
+}
+
+/* ボタンの文字 */
+#GameOver .menus .btext {
+    font-size: 1.4rem;
+    color: white;
+    /* -webkit-text-stroke: 3px rgb(255, 230, 0); */
 }
 
 /* StageClear */
+/* タイトル */
 #StageClear .title {
-    color: rgb(255, 174, 0);
-    -webkit-text-stroke: 3px rgb(255, 230, 0);
-
+    font-size: 5.5rem;
+    color: #ffbb00;
+    -webkit-text-stroke: 2px rgb(255, 255, 255);
 }
 
+/* ボタンの枠 */
+#StageClear .menus .box {
+    width: 100%;
+    height: 25%;
+    margin: 10px auto;
+    border: 1px #000 solid;
+    background-color: rgb(35, 174, 255);
+}
 
-
-
+/* ボタンの文字 */
+#StageClear .menus .btext {
+    font-size: 1.4rem;
+    color: white;
+    /* -webkit-text-stroke: 3px rgb(255, 230, 0); */
+}
 </style>
--- src/views/menu.vue
+++ src/views/menu.vue
@@ -4,12 +4,18 @@
       <p id="title" class="titles">Tower Attack Game</p>
       <p id="subtitle" class="titles">ステージセレクト</p>
       <div id="stages">
-        <RouterLink class="RouterLink" :to="'/game/' + StageID" v-for="StageID in Stages">
+        <a class="RouterLink" :href="'/game/' + StageID" v-for="StageID in Stages">
           <div class="stage">
             <p class="text Name"><b>{{ StageData[StageID]["StageName"] }}</b></p>
             <p class="text Description">{{ StageData[StageID]["Description"] }}</p>
           </div>
-        </RouterLink>
+        </a>
+        <!-- <RouterLink class="RouterLink" :to="'/game/' + StageID" v-for="StageID in Stages">
+          <div class="stage">
+            <p class="text Name"><b>{{ StageData[StageID]["StageName"] }}</b></p>
+            <p class="text Description">{{ StageData[StageID]["Description"] }}</p>
+          </div>
+        </RouterLink> -->
       </div>
     </div>
   </div>
