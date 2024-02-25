22b94d5da30188a9a8146c6b3325de7a89ca70df
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-02-26 00:28:29 +0900

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-
+import VueGtag from "vue-gtag";
 
 // FontAwesome
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -11,5 +11,10 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 library.add(fas, fab, far);
 
+// Google Analytics
 
-createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App)
+    .use(router)
+    .use(VueGtag, { config: { id: "G-5RR4CY9KB3" } })
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
